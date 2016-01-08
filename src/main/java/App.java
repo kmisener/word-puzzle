73021ae566a8c3f7fd479b79d89f3dbadab9d110
kyle misener <km.pacific.27@gmail.com>
2016-01-07 22:42:38 -0800

    Add image for main html page layout

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    //staticFileLocation("/public");
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
