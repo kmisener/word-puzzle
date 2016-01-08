4a0b97ef117eef0ce4abb7776ebe480c8ae944e6
kyle misener <km.pacific.27@gmail.com>
2016-01-07 22:24:31 -0800

    Add main page html layout

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
   public static void main(String[] args) {
+    //staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+
+      model.put("template", "templates/index.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
