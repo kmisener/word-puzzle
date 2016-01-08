05bdcd06d7c9984fd1b0da3e5076167b66096fb9
kyle misener <km.pacific.27@gmail.com>
2016-01-07 21:17:22 -0800

    Add unit test for letter E/e

--- src/main/java/WordPuzzleCreator.java
+++ src/main/java/WordPuzzleCreator.java
@@ -1,10 +1,18 @@
 public class WordPuzzleCreator {
 
   public String createWordPuzzle(String word) {
+
     String wordPuzzle = "";
+
     for(int i = 0; i < word.length(); i++) {
       if(word.charAt(i) == 'a' || word.charAt(i) == 'A') {
         wordPuzzle += "-";
+      }
+      else if(word.charAt(i) == 'e' || word.charAt(i) == 'E'){
+        wordPuzzle += "-";
+      }
+      else {
+        wordPuzzle += word.substring(i, i+1);
       }
     }
 
--- src/test/java/CreateWordPuzzleTest.java
+++ src/test/java/CreateWordPuzzleTest.java
@@ -21,4 +21,14 @@ public class CreateWordPuzzleTest {
   public void createWordPuzzle_inputAReturnDash_Dash(){
     assertEquals("-", wordPuzzle.createWordPuzzle("A"));
   }
+
+  @Test
+  public void createWordPuzzle_inputeReturnDash_Dash(){
+    assertEquals("-", wordPuzzle.createWordPuzzle("e"));
+  }
+
+  @Test
+  public void createWordPuzzle_inputEReturnDash_Dash(){
+    assertEquals("-", wordPuzzle.createWordPuzzle("E"));
+  }
 }
