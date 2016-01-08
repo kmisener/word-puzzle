863759555b45d6bbaa80a98c9f534d0acefe5b89
kyle misener <km.pacific.27@gmail.com>
2016-01-08 08:44:05 -0800

    Add output page for word converted to puzzle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,13 +5,28 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+
     staticFileLocation("/public");
+
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
       HashMap model = new HashMap();
 
       model.put("template", "templates/index.vtl");
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/output", (request, response) -> {
+      HashMap model = new HashMap();
+      WordPuzzleCreator wordPuzzleCreator = new WordPuzzleCreator();
+
+      String word = request.queryParams("word");
+      String wordPuzzle = wordPuzzleCreator.createWordPuzzle(word);
+
+      model.put("template", "templates/output.vtl");
+      model.put("word", word);
 
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
